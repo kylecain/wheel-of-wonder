f1b788441b6c881ce7f753c839adcc898ac4f1ff
Kyle Cain <kylecain0305@gmail.com>
2025-08-28 17:18:03 -0400

    update output of all movies command

--- internal/command/all_movies.go
+++ internal/command/all_movies.go
@@ -3,6 +3,7 @@ package command
 import (
 	"fmt"
 	"log/slog"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/kylecain/wheel-of-wonder/internal/db/repository"
@@ -32,10 +33,25 @@ func (c *AllMovies) Handler(s *discordgo.Session, i *discordgo.InteractionCreate
 		return
 	}
 
-	response := "Movies in the wheel:\n"
+	maxIDLen := len("ID")
+	maxTitleLen := len("Movie")
 	for _, movie := range movies {
-		response += fmt.Sprintf("- ID: %d - Title: %s\n", movie.ID, movie.Title)
+		idLen := len(fmt.Sprintf("%d", movie.ID))
+		if idLen > maxIDLen {
+			maxIDLen = idLen
+		}
+		if len(movie.Title) > maxTitleLen {
+			maxTitleLen = len(movie.Title)
+		}
 	}
+
+	response := "```" +
+		fmt.Sprintf("| %-*s | %-*s |\n", maxIDLen, "ID", maxTitleLen, "Movie") +
+		fmt.Sprintf("|%s|%s|\n", strings.Repeat("-", maxIDLen+2), strings.Repeat("-", maxTitleLen+2))
+	for _, movie := range movies {
+		response += fmt.Sprintf("| %-*d | %-*s |\n", maxIDLen, movie.ID, maxTitleLen, movie.Title)
+	}
+	response += "```"
 
 	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
